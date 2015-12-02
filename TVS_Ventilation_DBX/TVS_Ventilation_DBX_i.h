7b58fcf76a5240c420c300a6638cd28eaf9e654b
Vitaut Tryputsin <tryputsin.v@gmail.com>
2015-12-02 21:49:26 +0300

    add correct paths for vs 2012

--- TVS_Ventilation_DBX/TVS_Ventilation_DBX_i.h
+++ TVS_Ventilation_DBX/TVS_Ventilation_DBX_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Tue Aug 18 11:41:58 2015
+/* at Wed Dec 02 21:39:49 2015
  */
 /* Compiler settings for TVS_Ventilation_DBX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
