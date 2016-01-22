87ce36fd58cc3d8becff5db10821265933c9feb6
Vitaut Tryputsin <tryputsin.v@gmail.com>
2016-01-22 11:42:38 +0300

    22.01.2016

--- TVS_Ventilation_DBX/TVS_Ventilation_DBX_i.h
+++ TVS_Ventilation_DBX/TVS_Ventilation_DBX_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Wed Jan 06 13:38:40 2016
+/* at Fri Jan 22 11:36:21 2016
  */
 /* Compiler settings for TVS_Ventilation_DBX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
