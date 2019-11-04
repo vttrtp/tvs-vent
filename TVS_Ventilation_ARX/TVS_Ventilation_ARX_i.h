0f1bb621fd6847c729d5d31c1c2fb13b65a718e4
Vitaut Tryputsin <tryputsin.v@gmail.com>
2019-11-04 11:17:04 +0300

    update

--- TVS_Ventilation_ARX/TVS_Ventilation_ARX_i.h
+++ TVS_Ventilation_ARX/TVS_Ventilation_ARX_i.h
@@ -3,11 +3,11 @@
 /* this ALWAYS GENERATED file contains the definitions for the interfaces */
 
 
- /* File created by MIDL compiler version 8.00.0595 */
-/* at Sun Nov 03 21:30:08 2019
+ /* File created by MIDL compiler version 8.00.0603 */
+/* at Mon Nov 04 10:24:45 2019
  */
 /* Compiler settings for TVS_Ventilation_ARX.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
@@ -21,7 +21,7 @@
 
 /* verify that the <rpcndr.h> version is high enough to compile this file*/
 #ifndef __REQUIRED_RPCNDR_H_VERSION__
-#define __REQUIRED_RPCNDR_H_VERSION__ 475
+#define __REQUIRED_RPCNDR_H_VERSION__ 500
 #endif
 
 #include "rpc.h"
--- TVS_Ventilation_DBX/TVS_Ventilation_DBX_i.h
+++ TVS_Ventilation_DBX/TVS_Ventilation_DBX_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Sun Nov 03 21:29:56 2019
+/* at Mon Nov 04 10:53:08 2019
  */
 /* Compiler settings for TVS_Ventilation_DBX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
